1374c19b8f0a23ee4e3c8905743ea0401ae68fb0
Ryan <962491243@qq.com>
2022-06-25 11:50:58 +0800

    bugfix

--- example/default.tsx
+++ example/default.tsx
@@ -4,11 +4,14 @@ export function App() {
   const word1 = t('Hello');
   const word2 = t(`word`);
   const word3 = t('Hello, word');
+  const word4 = t(`The subscriber will see this email as the FROM address of the email.
+  You will receive a confirmation email if you haven't confirmed this address earlier.`);
   return (
     <div>
       {word1}
       {word2}
       {word3}
+      {word4}
     </div>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,13 @@ export function extract(config: Config) {
   const grep = os.platform() === 'darwin' ? 'ggrep' : 'grep';
   const words: string[] = [];
   const tempFile = path.join(process.cwd(), `./_translate${+new Date()}.txt`);
-  const result = shell.exec(
-    `${grep} -Phrzoe "(?<=[^a-zA-Z]${keyword}\\()[\\n\\s\\t]*([\\"\\\`\\'])([\\d\\D]*?)(?=\\1([\\n\\s]*)(\\)|,))" ${config.path} | ${grep} -Phzoe "(?<=[\\"\\\`\\']).*" > ${tempFile}`,
-  );
-  if (result.code === 1) {
-    fs.rmSync(tempFile);
-    console.log('\x1b[31m', 'No match content');
-    return [];
-  }
-  if (result.code !== 0) throw new Error(result.stderr);
-
+  shell
+    .exec(
+      `${grep} -Phrzoe "(?<=[^a-zA-Z]${keyword}\\()[\\n\\s\\t]*([\\"\`\\'])([\\d\\D]*?)\\1(?=([\\n\\s\\t]*)[\\),])" ${config.path}`,
+      { silent: true },
+    )
+    .exec(`${grep} -Pzoe "(?<=([\\"\\\`\\']))[\\d\\D]*(?=\\1)" `, { silent: true })
+    .to(tempFile);
   const content = fs.readFileSync(tempFile, {
     encoding: 'utf-8',
   });
